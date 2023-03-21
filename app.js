94676397441203df0783a5b2daf418129cab63ec
Covstar <okechukwuvictor093@gmail.com>
2023-03-21 12:08:34 +0100

    Created my first nodejs template with tailwindcss and i love it

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser");
+const app = express();
+
+// static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+
+
+// set templating engine 
+app.use(expressLayouts)
+app.set('layout', './layouts/full-width')
+app.set('view engine', 'ejs')
+
+app.get("/", function(req, res) {
+    res.render('index', {
+        title: 'Home page'
+    });
+});
+
+app.get("/about", function(req, res) {
+    res.render('about', {
+        title: 'About page',
+        layout: './layouts/sidebar'
+    });
+});
+
+
+
+
+
+
+
+
+app.listen(5000, function(){
+    console.log("Started listening at port 5000");
+})
