736f73576d3bdcce0bbc05d11ee737d7c5215981
Covstar <okechukwuvictor093@gmail.com>
2023-03-21 14:41:11 +0100

    deleted some things

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 
 // static files
-app.use(express.static('./public'))
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(__dirname + 'public'))
+app.use(express.static('./public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 // set templating engine 
